537766a8033a23793eddcd3a66bf637196dbff1f
Eduardo Jose Martinez Lam <eduardo.martinez@bsocorp.com>
2022-10-17 09:37:54 -0500

    Ajuste porque el BCryptPasswordEncoder no acepta valores menores a 4

--- src/main/java/com/mdf/springjpa/Spring/Jpa/config/ProyectSecurityConfig.java
+++ src/main/java/com/mdf/springjpa/Spring/Jpa/config/ProyectSecurityConfig.java
@@ -3,6 +3,7 @@ package com.mdf.springjpa.Spring.Jpa.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,6 +20,7 @@ public class ProyectSecurityConfig {
 
 //		Permit just what you want on matches and csrf permit post for all user
 		http.authorizeRequests().antMatchers("/api/**").authenticated().and().formLogin().and().httpBasic().and().csrf().disable();
+//		http.authorizeRequests().antMatchers("/api/**").permitAll().and().formLogin().and().httpBasic().and().csrf().disable();
 		return http.build();
 	}
 
@@ -29,8 +31,9 @@ public class ProyectSecurityConfig {
 //	}
 
 	//	Es el mas basico para codificar una contraseña pero no es la mas segura.
+	//	El parametro de BCryptPasswordEncoder solo acepta valores mayores a 4
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder(2);		
+		return new BCryptPasswordEncoder(5);		
 	}
 }
--- src/main/java/com/mdf/springjpa/Spring/Jpa/config/StudentDetailsConfig.java
+++ src/main/java/com/mdf/springjpa/Spring/Jpa/config/StudentDetailsConfig.java
@@ -3,39 +3,70 @@ package com.mdf.springjpa.Spring.Jpa.config;
 import java.util.ArrayList;
 import java.util.List;
 
+//import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.mdf.springjpa.Spring.Jpa.models.Student;
 import com.mdf.springjpa.Spring.Jpa.repository.IStudentRepository;
 
 @Service
-public class StudentDetailsConfig implements UserDetailsService {
+public class StudentDetailsConfig implements AuthenticationProvider {
 
 	@Autowired
 	private IStudentRepository _studentRepository;
 
+	@Autowired
+	private PasswordEncoder _passwordEncoder;
+
+//	@Override
+//	public Authentication loadUserByUsername(String username) throws UsernameNotFoundException {
+//		String userName, password = null;
+//		List<GrantedAuthority> authorities = null;
+//		Student students = _studentRepository.findByEmailId(username);
+//		if (students == null)
+//			throw new UsernameNotFoundException("User details not found.");
+//		else {
+////			userName = students.getEmailId();
+////			password = students.getPassword();
+////			authorities = new ArrayList<>();
+////			authorities.add(new SimpleGrantedAuthority(students.getRole()));
+//		}
+//		return null;
+//	}
+
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		String userName, password = null;
-		List<GrantedAuthority> authorities = null;
-		Student students = _studentRepository.findByEmailId(username);
-		System.out.println(students);
-		if (students == null)
-			throw new UsernameNotFoundException("User details not found.");
-		else {
-			userName = students.getEmailId();
-			password = students.getPassword();
-			authorities = new ArrayList<>();
-			authorities.add(new SimpleGrantedAuthority(students.getRole()));
+	public org.springframework.security.core.Authentication authenticate(
+			org.springframework.security.core.Authentication authentication) throws AuthenticationException {
+		String userName = authentication.getName();
+		String password = authentication.getCredentials().toString();
+		Student students = this._studentRepository.findByEmailId(userName);
+		if (students != null) {
+			if (_passwordEncoder.matches(password, students.getPassword())) {
+				List<GrantedAuthority> authorities = new ArrayList<>();
+				authorities.add(new SimpleGrantedAuthority(students.getRole()));
+				return new UsernamePasswordAuthenticationToken(userName, password, authorities);
+			} else {
+				throw new BadCredentialsException("No user registered with those credentials");
+			}
 		}
-		return new User(userName, password, authorities);
+		return null;
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
 	}
 
 }
